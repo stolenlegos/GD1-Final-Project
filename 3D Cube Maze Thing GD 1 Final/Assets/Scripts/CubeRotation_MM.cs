67e67b7b97b28f5cadba48d79429bec3b34138d4
stolenlegos <stolenlegos@gmail.com>
2021-04-09 11:56:40 -0400

    Cube Rotation
    using arrow keys, prototype

--- 3D Cube Maze Thing GD 1 Final/Assets/Scripts/CubeRotation_MM.cs
+++ 3D Cube Maze Thing GD 1 Final/Assets/Scripts/CubeRotation_MM.cs
@@ -25,22 +25,34 @@ public class CubeRotation_MM : MonoBehaviour
         StartCoroutine(Rotate(Vector3.forward, -90, 1.0f));
         playerRestriction = 1.2f;
       }
+
+      if(Input.GetKeyDown(KeyCode.UpArrow) && playerRestriction <= 0)
+      {
+        StartCoroutine(Rotate(Vector3.right, 90, 1.0f));
+        playerRestriction = 1.2f;
+      }
+
+      if(Input.GetKeyDown(KeyCode.DownArrow) && playerRestriction <= 0)
+      {
+        StartCoroutine(Rotate(Vector3.right, -90, 1.0f));
+        playerRestriction = 1.2f;
+      }
+
       playerRestriction -= Time.deltaTime;
     }
 
     IEnumerator Rotate(Vector3 axis, float angle, float duration = 1.0f)
     {
-      Quaternion from = transform.localRotation;
-      Quaternion to = transform.localRotation;
-      to *= Quaternion.Euler(axis * angle);
+      Quaternion from = transform.rotation;
+      Quaternion to = Quaternion.Euler(axis * angle) * transform.rotation;
 
       float elapsed = 0.0f;
       while(elapsed < duration)
       {
-        transform.localRotation = Quaternion.Slerp(from, to, elapsed / duration);
+        transform.rotation = Quaternion.Slerp(from, to, elapsed / duration);
         elapsed += Time.deltaTime;
         yield return null;
       }
-      transform.localRotation = to;
+      transform.rotation = to;
     }
 }
