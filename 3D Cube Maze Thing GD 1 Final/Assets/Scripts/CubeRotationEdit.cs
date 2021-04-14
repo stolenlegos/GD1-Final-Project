cc2482c3a61011ac9856410a47f45c08f4e6a0b4
CoralineDark <49164730+CoralineDark@users.noreply.github.com>
2021-04-14 03:37:43 -0400

    turning UI added, teleporters tbc

---
+++ 3D Cube Maze Thing GD 1 Final/Assets/Scripts/CubeRotationEdit.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRotationEdit : MonoBehaviour
+{
+  float playerRestriction;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+      playerRestriction -= Time.deltaTime;
+    }
+
+    public void turnDown() { 
+        StartCoroutine(Rotate(Vector3.right, -90, 1.0f));
+        playerRestriction = 1.2f;
+    }
+
+    public void turnUp() { 
+        StartCoroutine(Rotate(Vector3.right, 90, 1.0f));
+        playerRestriction = 1.2f;
+    }
+
+    public void turnLeft() { 
+        StartCoroutine(Rotate(Vector3.forward, 90, 1.0f));
+        playerRestriction = 1.2f;
+    }
+
+    public void turnRight() { 
+        StartCoroutine(Rotate(Vector3.forward, -90, 1.0f));
+        playerRestriction = 1.2f;
+    }
+
+    public void dropPlayer() { 
+        turnUp();
+        GameObject player = GameObject.Find("player"); 
+        Rigidbody rb = player.GetComponent<Rigidbody>(); 
+        rb.useGravity = true; 
+    }
+
+    IEnumerator Rotate(Vector3 axis, float angle, float duration = 1.0f)
+    {
+      Quaternion from = transform.rotation;
+      Quaternion to = Quaternion.Euler(axis * angle) * transform.rotation;
+
+      float elapsed = 0.0f;
+      while(elapsed < duration)
+      {
+        transform.rotation = Quaternion.Slerp(from, to, elapsed / duration);
+        elapsed += Time.deltaTime;
+        yield return null;
+      }
+      transform.rotation = to;
+    }
+}
