cf93815a3cc67dfba2afc5aefc3f37d523c8cb48
solidsmyth <74252115+solidsmyth@users.noreply.github.com>
2021-04-19 21:51:24 -0400

    Initial Turret Updates

---
+++ 3D Cube Maze Thing GD 1 Final/Assets/Scripts/Sam's Midterm Scripts/bulletScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletScript : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + transform.forward * speed * Time.deltaTime; 
+    }
+    void OnTriggerEnter(Collider col)
+    {
+
+        if (col.gameObject.tag == "maze")
+        {
+            Destroy(gameObject);
+        }
+
+        if (col.gameObject.tag == "BreakableWall")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
