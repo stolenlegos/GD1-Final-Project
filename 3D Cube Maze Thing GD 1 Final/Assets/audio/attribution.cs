16c23e61aad37cbf51e26d8008c779ba0760d4aa
dagarber <74267974+dagarber@users.noreply.github.com>
2021-04-21 16:00:13 -0400

    added attribution for audio and lowered volume in game to 0.005

---
+++ 3D Cube Maze Thing GD 1 Final/Assets/audio/attribution.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attribution : MonoBehaviour
+{
+    // Audio source is freesound.org, with Attribution license:
+    // 251461__joshuaempyre__arcade-music-loop.wav
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
