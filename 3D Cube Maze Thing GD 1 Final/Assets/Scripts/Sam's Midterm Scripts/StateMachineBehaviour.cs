9f68a8cb8705b95fe7fe353b382becc6331a9e90
solidsmyth <74252115+solidsmyth@users.noreply.github.com>
2021-04-21 20:54:54 -0400

    turret changes

--- 3D Cube Maze Thing GD 1 Final/Assets/Scripts/Sam's Midterm Scripts/StateMachineBehaviour.cs
+++ 3D Cube Maze Thing GD 1 Final/Assets/Scripts/Sam's Midterm Scripts/StateMachineBehaviour.cs
@@ -14,7 +14,7 @@ public class StateMachineBehaviour : MonoBehaviour
     }
   States state = States.Idle; // Idle = 0, Fire = 1, Search = 2
 
-    public GameObject bulletPrefab;
+  public GameObject bulletPrefab;
 
   public Transform target;
 
@@ -43,14 +43,17 @@ public class StateMachineBehaviour : MonoBehaviour
         player_dist = Vector3.Distance(target.position, transform.position); 
 
         Vector3 targetDir = target.position - transform.position;
-        angleBetween = Vector3.Angle(-transform.forward, targetDir);
+        angleBetween = Vector3.Angle(transform.forward, targetDir);
+
 
         bool wallBlocksVisibility = false;
 
         RaycastHit hitInfo;
-        if(Physics.Raycast(transform.position, targetDir.normalized, out hitInfo, targetDir.magnitude))
+        if (Physics.Raycast(transform.position, targetDir.normalized, out hitInfo, targetDir.magnitude))
+
+            Debug.Log("Working");
         {
-            if(hitInfo.collider.gameObject != target)
+            if(hitInfo.collider.gameObject.tag != "player")
             {
                 wallBlocksVisibility = true;
             }
@@ -59,12 +62,18 @@ public class StateMachineBehaviour : MonoBehaviour
         bool player_in_cone = player_dist <= player_dist_threshold && angleBetween >= -angleThreshold && angleBetween <= angleThreshold && !wallBlocksVisibility;
 
         Debug.DrawRay(transform.position, targetDir.normalized * player_dist_threshold, Color.red);
-        Debug.DrawRay(transform.position, -transform.forward * player_dist_threshold, Color.blue);
+        Debug.DrawRay(transform.position, transform.forward * player_dist_threshold, Color.blue);
+
+        if(player_dist > player_dist_threshold)
+        {
+            Debug.Log("player is not in range");
+        }
 
         if(state == States.Idle && player_in_cone)
         {
             state = States.Fire;
 
+            Debug.Log("player in range");
         }
         else if(state == States.Fire && !player_in_cone)
         {
@@ -80,30 +89,32 @@ public class StateMachineBehaviour : MonoBehaviour
         }
         if(state == States.Idle)
         {
-            
+            Debug.Log("Turret is idle");
         }
         if (state == States.Fire)
         {
             GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
             if (angleBetween < 0.0f)
             {
-                transform.Rotate(Vector3.up, -1.0f, Space.Self);
+                transform.Rotate(Vector3.up, -0.01f, Space.Self);
 
-                currentRotation = currentRotation - 1.0f;
+                currentRotation = currentRotation - 0.01f;
             }
             else
             {
-                transform.Rotate(Vector3.up, 1.0f, Space.Self);
+                transform.Rotate(Vector3.up, 0.01f, Space.Self);
 
                 currentRotation = currentRotation + 1;
             }
+
+            Debug.Log("Turret is firing");
      
         }
         if(state == States.Search)
         {
             if(MovingRight && currentRotation < angleThreshold)
             {
-                transform.Rotate(Vector3.up, 1.0f, Space.Self);
+                transform.Rotate(Vector3.up, 0.01f, Space.Self);
 
                 currentRotation = currentRotation + 1;
             }
@@ -113,7 +124,7 @@ public class StateMachineBehaviour : MonoBehaviour
             }
             else if (currentRotation > -angleThreshold)
             {
-                transform.Rotate(Vector3.up, -1.0f, Space.Self);
+                transform.Rotate(Vector3.up, -0.01f, Space.Self);
 
                 currentRotation = currentRotation - 1.0f;
             }
@@ -123,6 +134,8 @@ public class StateMachineBehaviour : MonoBehaviour
             }
 
             FrameTimer = FrameTimer - 1;
+
+            Debug.Log("Turret is searching");
         }
  
     }
