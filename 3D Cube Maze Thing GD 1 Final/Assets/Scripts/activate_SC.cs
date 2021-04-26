227a530317acc4ff39933c6b13deaeba08fec068
CoralineDark <49164730+CoralineDark@users.noreply.github.com>
2021-04-26 00:22:47 -0400

    ahahahahhahahaha

--- 3D Cube Maze Thing GD 1 Final/Assets/Scripts/activate_SC.cs
+++ 3D Cube Maze Thing GD 1 Final/Assets/Scripts/activate_SC.cs
@@ -8,6 +8,7 @@ public class activate_SC : MonoBehaviour
     public GameObject player;
     
     public void OnTriggerEnter(Collider other) { 
+        print("collision");
         if (other.CompareTag("teleportSpot")) { 
             if(!player.GetComponent<SC_MovingPlatform>().enabled)
                 player.GetComponent<SC_MovingPlatform>().enabled = true;
