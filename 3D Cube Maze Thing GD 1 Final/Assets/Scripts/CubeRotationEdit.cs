341b09c9bb9f9aa84a9ea3bd1afe780494a27c5b
CoralineDark <49164730+CoralineDark@users.noreply.github.com>
2021-04-26 00:10:56 -0400

    Update CubeRotationEdit.cs

--- 3D Cube Maze Thing GD 1 Final/Assets/Scripts/CubeRotationEdit.cs
+++ 3D Cube Maze Thing GD 1 Final/Assets/Scripts/CubeRotationEdit.cs
@@ -73,7 +73,6 @@ public class CubeRotationEdit : MonoBehaviour
           allowFall = false;
           player.GetComponent<CharacterMove>().enabled = true;
           player.GetComponent<CharacterController>().enabled = true;
-          player.GetComponent<SC_MovingPlatform>().enabled = true;
           mainCamera.GetComponent<cameraMove>().enabled = true;
       }
       allowButton = true; 
