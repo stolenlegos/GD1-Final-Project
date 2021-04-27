c86274ac77daa50f78a206739ac4d185c2b44eef
stolenlegos <stolenlegos@gmail.com>
2021-04-27 15:43:05 -0400

    Win State push

--- 3D Cube Maze Thing GD 1 Final/Assets/Scripts/levelChipScripts.cs
+++ 3D Cube Maze Thing GD 1 Final/Assets/Scripts/levelChipScripts.cs
@@ -5,44 +5,44 @@ using UnityEngine;
 public class levelChipScripts : MonoBehaviour
 {
     public bool greenWon;
-    private bool greenActive; 
-    public GameObject greenChip; 
+    private bool greenActive;
+    public GameObject greenChip;
     public bool blueWon;
-    private bool blueActive;  
+    private bool blueActive;
     public GameObject blueChip;
     public bool yellowWon;
-    
-    private bool yellowActive; 
-    public GameObject yellowChip; 
+
+    private bool yellowActive;
+    public GameObject yellowChip;
     public bool purpleWon;
-    
-    private bool purpleActive;  
-    public GameObject purpleChip; 
+
+    private bool purpleActive;
+    public GameObject purpleChip;
     public bool redWon;
-    
-    private bool redActive; 
+
+    private bool redActive;
     public GameObject redChip;
-    private bool whiteActive; 
-    public GameObject whiteWall;  
+    private bool whiteActive;
+    public GameObject whiteWall;
     // Start is called before the first frame update
     void Start()
     {
-        greenChip.SetActive(false); 
+        greenChip.SetActive(false);
         blueChip.SetActive(false);
-        yellowChip.SetActive(false); 
-        purpleChip.SetActive(false); 
-        redChip.SetActive(false);  
-        greenWon = false; 
-        greenActive = false; 
-        blueWon = false; 
+        yellowChip.SetActive(false);
+        purpleChip.SetActive(false);
+        redChip.SetActive(false);
+        greenWon = false;
+        greenActive = false;
+        blueWon = false;
         blueActive = false;
-        yellowWon = false; 
-        yellowActive = false; 
-        purpleWon = false; 
-        purpleActive = false; 
-        redWon = false; 
-        redActive = false; 
-        whiteActive = true;  
+        yellowWon = false;
+        yellowActive = false;
+        purpleWon = false;
+        purpleActive = false;
+        redWon = false;
+        redActive = false;
+        whiteActive = true;
     }
 
     // Update is called once per frame
@@ -50,7 +50,7 @@ public class levelChipScripts : MonoBehaviour
     {
         if (greenWon && !greenActive)
             setGreenActive();
-        if (blueWon && !blueActive) 
+        if (blueWon && !blueActive)
             setBlueActive();
         if (yellowWon && !yellowActive)
             setYellowActive();
@@ -59,32 +59,32 @@ public class levelChipScripts : MonoBehaviour
         if (redWon && !redActive)
             setRedActive();
         if (greenWon && blueWon && yellowWon && purpleWon && redWon && whiteActive)
-            openExit(); 
+            openExit();
     }
 
-    private void setGreenActive() { 
-        greenActive = true; 
-        greenChip.SetActive(true); 
+    private void setGreenActive() {
+        greenActive = true;
+        greenChip.SetActive(true);
     }
-    private void setBlueActive() { 
-        blueActive = true; 
-        blueChip.SetActive(true); 
+    private void setBlueActive() {
+        blueActive = true;
+        blueChip.SetActive(true);
     }
-    private void setPurpleActive() { 
-        blueActive = true; 
-        purpleChip.SetActive(true); 
+    private void setPurpleActive() {
+        purpleActive = true; 
+        purpleChip.SetActive(true);
     }
-    private void setYellowActive() { 
-        yellowActive = true; 
-        yellowChip.SetActive(true); 
+    private void setYellowActive() {
+        yellowActive = true;
+        yellowChip.SetActive(true);
     }
-    private void setRedActive() { 
-        redActive = true; 
-        redChip.SetActive(true); 
+    private void setRedActive() {
+        redActive = true;
+        redChip.SetActive(true);
     }
 
-    private void openExit() { 
-        whiteActive = false; 
-        whiteWall.SetActive(false); 
+    private void openExit() {
+        whiteActive = false;
+        whiteWall.SetActive(false);
     }
 }
