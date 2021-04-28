7dea68f19e810f1dff4481b96d3f95381573b7be
dagarber <74267974+dagarber@users.noreply.github.com>
2021-04-28 11:00:29 -0400

    Revert "Revert "script updates""
    This reverts commit 3df4cc311df3e01319b0047ab547241fc9676b93.

---
+++ 3D Cube Maze Thing GD 1 Final/Assets/Scripts/dust_storm.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dust_storm : MonoBehaviour
+{
+
+    public float speed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(0, speed * Time.deltaTime, 0);
+    }
+}
